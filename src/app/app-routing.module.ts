d899e469f9f95cbd32fb98802bb9f5621ae63ff4
Vladislav <papoakr@gmail.com>
2024-03-25 14:33:03 +0500

    added baset page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'catalog', loadChildren: () => import('./modules/pages/catalog/catalog.module').then(m => m.CatalogModule) }
+  { path: 'catalog', loadChildren: () => import('./modules/pages/catalog/catalog.module').then(m => m.CatalogModule) },
+  { path: 'basket', loadChildren: () => import('./modules/pages/basket/basket.module').then(m => m.BasketModule) },
 ];
 
 @NgModule({
